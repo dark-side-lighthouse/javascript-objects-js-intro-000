d1bcf125b9a5f6512f66037ee56ac997803a2244
ana.rajner@gmail.com <ana.rajner@gmail.com>
2018-03-15 10:56:36 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,6 @@
 const playlist = {artistName: 'songTitle'};
 
 function updatePlaylist(playlist, artistName, songTitle) {
+  playlist.artistName = 'songTitle';
   return Object.assign({}, playlist, {[key]:value})
 }
